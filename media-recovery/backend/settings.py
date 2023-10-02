89f51a004ab796320389600b36fd690fdac02fbd
Edson Carlos Garcia Junior6 <ejunior6@Lenovo.com>
2023-10-02 10:00:33 -0300

    media-03

--- media-recovery/backend/settings.py
+++ media-recovery/backend/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static/',
+    BASE_DIR / 'global_static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- media-recovery/machineInfo/views.py
+++ media-recovery/machineInfo/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request,
                   'clone/pages/home.html',
                   context={
-                      'serialNumber': '128B12',
+                      'Model': 'LENOVO V330-15IKB',
                   })
 
 
@@ -15,5 +15,11 @@ def item(request, id):
     return render(request,
                   'clone/pages/item.html',
                   context={
-                      'serialNumber': '128B12',
+                      'Product': '81AX012WJP',
+                      'Model': 'LENOVO V330-15IKB',
+                      'SerialNumber': 'R90T1QLE',
+                      'MachineTypeNumber': '81AX012WJP',
+                      'BIOSVersion': '6SCN41WW',
+                      'BIOSReleaseDate': '2023-09-28T13:43:59.077Z',
+
                   })
